fa9c59cafffa91c5f457457cd6aa3141c434dfdd
Robin Yoong <robinyoong@Robins-MacBook-Pro.local>
2022-05-23 18:33:14 +0800

    add cloudinary image

---
+++ components/NuxtImage.vue
@@ -0,0 +1,7 @@
+<template>
+  <nuxt-img
+    provider="cloudinary"
+    src="/v1653300395/super-nintendo_qj256q.jpg"
+    width="300"
+  />
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtImage />
+</template>
+
+<script>
+import NuxtImage from '~/components/NuxtImage.vue'
+export default {
+  name: 'IndexPage',
+  components: { NuxtImage },
+}
+</script>
